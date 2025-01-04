710cb88f5ea8c20f70e3f155b52913f695c198af
D. Scarnecchia <scarnecchia@gmail.com>
2025-01-03 20:08:23 -0500

    Minor fix

--- src/checkPosts.ts
+++ src/checkPosts.ts
@@ -57,12 +57,14 @@ export const checkPosts = async (post: Post[]) => {
                 post[0].did,
                 `${post[0].time}: ${checkPost?.comment} at ${post[0].atURI} with text "${post[0].text}"`,
               );
-            } else if (checkPost?.label !== "fundraising-link") {
-              // skip fundraising links—hardcoded because of the insane volume by spammers.
-              createAccountReport(
-                post[0].did,
-                ` ${post[0].time}: ${checkPost!.comment} at ${post[0].atURI} with text "${post[0].text}"`,
-              );
+            } else {
+              if (checkPost?.label !== "fundraising-link") {
+                // skip fundraising links—hardcoded because of the insane volume by spammers.
+                createAccountReport(
+                  post[0].did,
+                  ` ${post[0].time}: ${checkPost!.comment} at ${post[0].atURI} with text "${post[0].text}"`,
+                );
+              }
             }
           }
         }
