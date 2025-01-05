225bbc592e9f2e60b39ce0d40bc0b9d0a748f034
D. Scarnecchia <scarnecchia@gmail.com>
2025-01-04 20:52:58 -0500

    updated regex

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,15 @@ import { Checks } from "./types.js";
 
 export const PROFILE_CHECKS: Checks[] = [
   {
+    label: "report-to-bsky",
+    comment: "Profile is being reported too frequently",
+    description: true,
+    displayName: false,
+    reportOnly: false,
+    commentOnly: true,
+    check: new RegExp("dobolen\\.eu\\.org|getallmylinks\\.com", "i"),
+  },
+  {
     label: "troll",
     comment: "Troll language found in profile",
     description: true,
@@ -181,7 +190,7 @@ export const POST_CHECKS: Checks[] = [
     reportOnly: false,
     commentOnly: false,
     check: new RegExp(
-      "gofundme\\.com|gofund\\.me|buymeacoffee\\.com|venmo\\.com|cash\\.app|cash\\.me|paypal\\.com|paypal\\.me|gogetfunding\\.com|winred\\.com|actblue\\.com|givesendgo\\.com|chuffed\\.org ",
+      "gofundme\\.com|gofund\\.me|buymeacoffee\\.com|venmo\\.com|cash\\.app|cash\\.me|paypal\\.com|paypal\\.me|gogetfunding\\.com|winred\\.com|actblue\\.com|givesendgo\\.com|chuffed\\.org",
       "i",
     ),
   },
