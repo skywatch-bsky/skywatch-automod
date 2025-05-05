53646db498a21e6929e8b279c3dea55b64b92508
D. Scarnecchia <scarnecchia@gmail.com>
2025-05-04 19:15:17 -0700

    Fixing something I broke

--- src/checkPosts.ts
+++ src/checkPosts.ts
@@ -24,44 +24,57 @@ export const checkPosts = async (post: Post[]) => {
     );
 
     if (checkPost?.ignoredDIDs) {
-      if (checkPost?.ignoredDIDs.includes(post[0].did)) {
-        logger.info(`Whitelisted DID: ${post[0].did}`);
+      if (checkPost?.ignoredDIDs.includes(did)) {
+        logger.info(`Whitelisted DID: ${did}`);
         return;
       }
 
-      if (checkPost!.check.test(post[0].text)) {
+      if (checkPost!.check.test(text)) {
         if (checkPost?.whitelist) {
-          if (checkPost?.whitelist.test(post[0].text)) {
+          if (checkPost?.whitelist.test(text)) {
             logger.info(`Whitelisted phrase found"`);
             return;
           }
         }
 
         if (checkPost!.reportOnly === true) {
-          logger.info(`${checkPost!.label} in post at ${post[0].atURI}`);
-          logger.info(`Report only: ${post[0].did}`);
+          logger.info(`${checkPost!.label} in post at ${atURI}`);
+          logger.info(`Report only: ${did}`);
           createAccountReport(
             did,
-            `${post[0].time}: ${checkPost?.comment} at ${post[0].atURI} with text "${post[0].text}"`,
+            `${time}: ${checkPost?.comment} at ${atURI} with text "${text}"`,
           );
           return;
-        }
+        } else {
+          logger.info(`Labeling post: ${atURI}`);
 
-        // Label Posts
-        logger.info(`Labeling post: ${post[0].atURI}`);
-        createPostLabel(
-          post[0].atURI,
-          post[0].cid,
-          `${checkPost!.label}`,
-          `${post[0].time}: ${checkPost!.comment} at ${post[0].atURI} with text "${post[0].text}"`,
-        );
-        if (checkPost!.commentOnly === true) {
-          logger.info(`Comment only: ${did}`);
-          createAccountComment(
-            post[0].did,
-            `${post[0].time}: ${checkPost?.comment} at ${post[0].atURI} with text "${post[0].text}"`,
+          createPostLabel(
+            post[0].atURI,
+            post[0].cid,
+            `${checkPost!.label}`,
+            `${post[0].time}: ${checkPost!.comment} at ${post[0].atURI} with text "${post[0].text}"`,
           );
-          return;
+          if (checkPost!.commentOnly === true) {
+            logger.info(`Comment only: ${post[0].did}`);
+            createAccountComment(
+              post[0].did,
+              `${post[0].time}: ${checkPost?.comment} at ${post[0].atURI} with text "${post[0].text}"`,
+            );
+            return;
+          } else if (
+            checkPost?.label === "fundraising-link" ||
+            checkPost?.label === "twitter-x"
+          ) {
+            return; // skip fundraising links—hardcoded because of the insane volume by spammers.
+          } else if (checkPost!.commentOnly === false) {
+            logger.info(
+              `Creating report for post ${post[0].atURI} on ${post[0].did}`,
+            );
+            createAccountReport(
+              post[0].did,
+              ` ${post[0].time}: ${checkPost!.comment} at ${post[0].atURI} with text "${post[0].text}"`,
+            );
+          }
         }
       }
     }
