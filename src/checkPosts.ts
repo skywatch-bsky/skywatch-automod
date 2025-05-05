17a6095732026f2ffcebd8bf0d5cf273df8a769e
D. Scarnecchia <scarnecchia@gmail.com>
2025-05-04 19:11:58 -0700

    Update constants

--- src/checkPosts.ts
+++ src/checkPosts.ts
@@ -24,42 +24,42 @@ export const checkPosts = async (post: Post[]) => {
     );
 
     if (checkPost?.ignoredDIDs) {
-      if (checkPost?.ignoredDIDs.includes(did)) {
-        logger.info(`Whitelisted DID: ${did}`);
+      if (checkPost?.ignoredDIDs.includes(post[0].did)) {
+        logger.info(`Whitelisted DID: ${post[0].did}`);
         return;
       }
 
-      if (checkPost!.check.test(text)) {
+      if (checkPost!.check.test(post[0].text)) {
         if (checkPost?.whitelist) {
-          if (checkPost?.whitelist.test(text)) {
+          if (checkPost?.whitelist.test(post[0].text)) {
             logger.info(`Whitelisted phrase found"`);
             return;
           }
         }
 
         if (checkPost!.reportOnly === true) {
-          logger.info(`${checkPost!.label} in post at ${atURI}`);
-          logger.info(`Report only: ${did}`);
+          logger.info(`${checkPost!.label} in post at ${post[0].atURI}`);
+          logger.info(`Report only: ${post[0].did}`);
           createAccountReport(
             did,
-            `${time}: ${checkPost?.comment} at ${atURI} with text "${text}"`,
+            `${post[0].time}: ${checkPost?.comment} at ${post[0].atURI} with text "${post[0].text}"`,
           );
           return;
         }
 
         // Label Posts
-        logger.info(`Labeling post: ${atURI}`);
+        logger.info(`Labeling post: ${post[0].atURI}`);
         createPostLabel(
-          atURI,
-          cid,
+          post[0].atURI,
+          post[0].cid,
           `${checkPost!.label}`,
-          `${time}: ${checkPost!.comment} at ${atURI} with text "${text}"`,
+          `${post[0].time}: ${checkPost!.comment} at ${post[0].atURI} with text "${post[0].text}"`,
         );
         if (checkPost!.commentOnly === true) {
           logger.info(`Comment only: ${did}`);
           createAccountComment(
-            did,
-            `${time}: ${checkPost?.comment} at ${atURI} with text "${text}"`,
+            post[0].did,
+            `${post[0].time}: ${checkPost?.comment} at ${post[0].atURI} with text "${post[0].text}"`,
           );
           return;
         }
