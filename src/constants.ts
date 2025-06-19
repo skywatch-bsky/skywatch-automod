3361736c632ea5ca54dd27d55761aa69880c6b6b
D. Scarnecchia <scarnecchia@gmail.com>
2025-06-19 00:02:08 -0400

    Updated constants

--- src/constants.ts
+++ src/constants.ts
@@ -108,7 +108,7 @@ export const PROFILE_CHECKS: Checks[] = [
     commentAcct: false,
     toLabel: true,
     check: new RegExp(
-      "#(MAGA|MAHA)\\b|#Trump ?202(4|8)🇺🇸?|((real|president|king|queen)? ?(barron|donald|eric|ivan(k)?a|tiffany|melenia) ?(john|j)? ?((trump)( |, )?(jr)?))\\b|(proud|king)magat?\\b|(trump ?(20(24|28|32|36|48)|(45|46|47|4547)|maga|god|jesus|lord|[0-9]{2,4}))\\b|potus(45|46|47)\\b|ultramagat?\\b|maga [0-9]{2,4}\\b|(trump ?(is)? ?(my|your)? ?(king|maga|train|daddy|army|nation|world))\\b",
+      "#?(MAGA|MAHA)\\b|#Trump ?202(4|8)🇺🇸?|((real|president|king|queen)? ?(barron|donald|eric|ivan(k)?a|tiffany|melenia) ?(john|j)? ?((trump)( |, )?(jr)?))\\b|(proud|king)magat?\\b|(trump ?(20(24|28|32|36|48)|(45|46|47|4547)|maga|god|jesus|lord|[0-9]{2,4}))\\b|potus(45|46|47)\\b|ultramagat?\\b|maga [0-9]{2,4}\\b|(trump ?(is)? ?(my|your)? ?(king|maga|train|daddy|army|nation|world))\\b",
       "i",
     ),
     whitelist: new RegExp(
@@ -138,7 +138,7 @@ export const PROFILE_CHECKS: Checks[] = [
     commentAcct: false,
     toLabel: true,
     check: new RegExp(
-      "#(MAGA|MAHA)\\b|#Trump ?2024🇺🇸?|#Trump20(24|28|32|36|48)|(proud|king)magat?|(trump ?(20(24|28|32|36|48)|(45|46|47|4547)|maga|god|jesus|lord|[0-9]{2,4}))|potus(45|46|47)|ultramagat?|maga [0-9]{2,4}|(trump ?(is)? ?(my|your)? ?(king|maga|train|daddy|army|nation|world))|(?:Vance2028|Vance([a-zA-Z]+)2028|Vance([a-zA-Z]+)28|Vance48)",
+      "#?(MAGA|MAHA)\\b|#Trump ?2024🇺🇸?|#Trump20(24|28|32|36|48)|(proud|king)magat?|(trump ?(20(24|28|32|36|48)|(45|46|47|4547)|maga|god|jesus|lord|[0-9]{2,4}))|potus(45|46|47)|ultramagat?|maga [0-9]{2,4}|(trump ?(is)? ?(my|your)? ?(king|maga|train|daddy|army|nation|world))|(?:Vance2028|Vance([a-zA-Z]+)2028|Vance([a-zA-Z]+)28|Vance48)",
       "i",
     ),
     whitelist: new RegExp(
