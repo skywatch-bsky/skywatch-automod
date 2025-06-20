32b3097cfa06740cbdf77e099f39a5b0eda34808
D. Scarnecchia <scarnecchia@gmail.com>
2025-06-19 20:05:04 -0400

    Update constants

--- src/checkPosts.ts
+++ src/checkPosts.ts
@@ -1,4 +1,4 @@
-import { LINK_SHORTENER, POST_CHECKS, langs } from "./constants.js";
+import { POST_CHECKS } from "./constants.js";
 import { Post } from "./types.js";
 import logger from "./logger.js";
 import {
@@ -7,7 +7,9 @@ import {
   createAccountComment,
   createPostReport,
 } from "./moderation.js";
-import { getFinalUrl, getLanguage } from "./utils.js";
+import { LINK_SHORTENER } from "./constants.js";
+import { getFinalUrl } from "./utils.js";
+
 export const checkPosts = async (post: Post[]) => {
   // Get a list of labels
   const labels: string[] = Array.from(
@@ -35,20 +37,11 @@ export const checkPosts = async (post: Post[]) => {
     }
   }
 
-  // Get the post's language
-  const lang = await getLanguage(post[0].text);
-
   // iterate through the labels
   labels.forEach((label) => {
     const checkPost = POST_CHECKS.find(
       (postCheck) => postCheck.label === label,
     );
-
-    if (label === "contains-slur" || label === "monitor-slur") {
-      if (!langs.includes(lang)) {
-        return;
-      }
-    }
 
     if (checkPost?.ignoredDIDs) {
       if (checkPost?.ignoredDIDs.includes(post[0].did)) {
