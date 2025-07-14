74666d2a608cfb4725d541bf0e70dfd385e6f3a6
D. Scarnecchia <scarnecchia@gmail.com>
2025-07-14 18:54:05 -0400

    update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { Checks } from "./types.js";
 
 export const LINK_SHORTENER = new RegExp(
-  "(?:https?:\\/\\/)?([^.]+\\.)?(tinyurl\\.com|bit\\.ly/|goo\\.gl|g\\.co/|ow\\.ly/|shorturl\\.at|t\\.co/|go\\.bsky\\.app|dlvrit\\.com/)",
+  "(?:https?:\\/\\/)?([^.]+\\.)?(tinyurl\\.com|bit\\.ly/|goo\\.gl|g\\.co/|ow\\.ly/|shorturl\\.at|t\\.co/|go\\.bsky\\.app|dlvrit\\.com|t\\.co|t\\.me|is\\.gd|v\\.gd)",
   "i",
 );
 
