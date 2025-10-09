5546c60e36a6925304ca6e08035fe622ebb63e6b
D. Scarnecchia <scarnecchia@gmail.com>
2025-10-09 16:43:20 -0400

    update constants

--- src/constants.ts
+++ src/constants.ts
@@ -665,7 +665,7 @@ export const POST_CHECKS: Checks[] = [
     reportPost: true,
     toLabel: false,
     check: new RegExp(
-      "only (2|two) genders((?=.*transition)(?=.*mental health challenges)(?=.*love)(?=.*ideology))|trump is (your ?)(king|god|jesus|daddy)|(there (are|is))? only (two|2) genders|Colony of New Twitter|bluecry|mutilate (adolescents|kids|children)|Floyd.*convicted|[Gg]eorge.*[Ff]entanyl|YWNBAW|CASTRATING Minors|\\b(?:https?:\\/\\/)?([^.]+\\.)?(?:x\\.com/libsoftiktok)|Genital Mutilation|2020 Election was Stolen|kiwifarms\\.st|Leftist terrorists|#IStandWthSandiePeggie|#SexMatters|#TheodoreUpton|radical leftists|demonrats",
+      "only (2|two) genders((?=.*transition)(?=.*mental health challenges)(?=.*love)(?=.*ideology))|trump is (your ?)(king|god|jesus|daddy)|(there (are|is))? only (two|2) genders|Colony of New Twitter|bluecry|mutilate (adolescents|kids|children)|Floyd.*convicted|[Gg]eorge.*[Ff]entanyl|YWNBAW|CASTRATING Minors|\\b(?:https?:\\/\\/)?([^.]+\\.)?(?:x\\.com/libsoftiktok)|2020 Election was Stolen|kiwifarms\\.st|Leftist terrorists|#IStandWthSandiePeggie|#SexMatters|#TheodoreUpton|radical leftists|demonrats",
       "i",
     ),
   },
@@ -677,7 +677,7 @@ export const POST_CHECKS: Checks[] = [
     reportPost: true,
     toLabel: false,
     check: new RegExp(
-      "protect women's sports|protect womens sports|Genital Mutilation|(trans)?gender ideology|biological males?|the transgender issue|trantifa",
+      "protect women's sports|protect womens sports|(trans)?gender ideology|biological males?|the transgender issue|trantifa",
       "i",
     ),
   },
