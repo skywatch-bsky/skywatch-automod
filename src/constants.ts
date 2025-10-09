0c0ebac7122cd0f541ad4572f3ead8ff281c0014
D. Scarnecchia <scarnecchia@gmail.com>
2025-10-09 11:15:25 -0400

    update constants

--- src/constants.ts
+++ src/constants.ts
@@ -163,7 +163,7 @@ export const PROFILE_CHECKS: Checks[] = [
       "i",
     ),
     whitelist: new RegExp(
-      "(#?(?<=Never|Fuck|anti|🚫|DNI|Remove|🖕))[ -:]{0,2}((#)?(Donald[ -:]?)?Trump|MAGA(t)?|DJT)|#?((Donald)?[ -:]?Trump[ -:]?Hater|magazine|stop[ -:]?project[ -:]?2025|MAGA[ :]DNI)",
+      "(#?(?<=Never|Fuck|anti|🚫|DNI|Remove|🖕|🚫NO))[ -:]{0,2}((#)?(Donald[ -:]?)?Trump|MAGA(t)?|DJT)|#?((Donald)?[ -:]?Trump[ -:]?Hater|magazine|stop[ -:]?project[ -:]?2025|MAGA[ :]DNI)",
       "iu",
     ),
     ignoredDIDs: [
